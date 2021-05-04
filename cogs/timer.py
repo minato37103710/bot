99735bced60022facea9b3313274ec7ffa74577b
orange <arigatoudane@outlook.jp>
2021-05-04 23:56:17 +0900

    a

--- cogs/timer.py
+++ cogs/timer.py
@@ -14,11 +14,23 @@ class timer(commands.Cog):
 
     @timer.command(name='second',aliases=['sec'])
     async def second(self,ctx,second):
-      await ctx.send(f'second秒タイマーをかけます')
+      await ctx.send(f'{second}秒タイマーをかけます')
       await asyncio.sleep(int(second))
       await ctx.send(f'{second}秒が経ちました')
 
+    @timer.command(name='minute',aliases=['mnt'])
+    async def second(self,ctx,minute):
+      await ctx.send(f'{minute}分タイマーをかけます')
+      minute = minute*60
+      await asyncio.sleep(int(minute))
+      await ctx.send(f'{minute}分が経ちました')
     
+    @timer.command(name='hour',aliases=['hu'])
+    async def second(self,ctx,hour):
+      await ctx.send(f'{hour}時間タイマーをかけます')
+      hour = hour*600
+      await asyncio.sleep(int(hour))
+      await ctx.send(f'{hour}時間が経ちました')
 
 def setup(bot):
 	bot.add_cog(timer(bot)) 
