4c720b47bfe4b5cc8bac5caa81755e678bf1bf07
orange <arigatoudane@outlook.jp>
2021-05-05 10:48:09 +0900

    pingふぁいるのついか

--- main.py
+++ main.py
@@ -17,26 +17,14 @@ db=TinyDB('db.json')
 
 User = Query()
 
-async def prefix_from_json(bot,message):
-    """
-    prefix.json を読み込んで該当するprefix（無ければ ! ）を返す。
-    """
-
-    async with aiofiles.open("prefix.json") as f:
-        contents = await f.read()
-    data = json.loads(contents)  # json形式として読み込む
-    return data.get(str(message.guild.id),",")
-
-bot = commands.Bot(command_prefix=prefix_from_json  # 定義した関数を渡しています
+bot = commands.Bot(command_prefix=','  # 定義した関数を渡しています
 ,
   cose_insensitive=True
 ,
   intents=intent
 ,
   help_command=None
-,
-  activity=discord.Game(f'標準prefixは[,]です。')
-  )
+)
 
 bot.author_id = 757106917947605034
 
@@ -95,25 +83,6 @@ async def on_command_error(ctx, error):
     print(ctx.author.guild)
 
 @bot.command()
-async def timerm(ctx,query):  #コマンドを判定
-    await ctx.send("timer start")
-    await asyncio.sleep(int(query) * 60)  #分を取得&60倍
-    await ctx.send(embed=discord.Embed(title='timer finished',description=f'{int(query)}'))  #内容を変換
-
-@bot.command()
-async def timerh(ctx,query):  #コマンドを判定
-    await ctx.send("timer start")
-    await asyncio.sleep(int(query) * 3600)  #分を取得&60倍
-    await ctx.send(embed=discord.Embed(title='timer finished',description=f'{int(query)}'))  #内容を変換
-
-@bot.command()
-async def timers(ctx,query):  #コマンドを判定
-    await ctx.send(f"{ctx.author.name} is timer start")
-    print(f'{ctx.guild}')
-    await asyncio.sleep(int(query))  #分を取得&60倍
-    await ctx.send(embed=discord.Embed(title='timer finished',description=f'{int(query)}'))  #内容を変換
-
-@bot.command()
 async def about(ctx):
     about = await ctx.send(embed=discord.Embed(title='loading...',description=''))
     await about.edit(embed=discord.Embed(title='timer bot stutas',description=f"{len(bot.guilds)}"))
@@ -141,16 +110,6 @@ async def shutdown(ctx):
     await sys.exit()
 
 @bot.command()
-async def my_status(ctx):
-    status=discord.Embed(title='your status',description=f'{ctx.author.status}',color=discord.Color.random())
-    await ctx.send(embed=status)
-
-@bot.command()
-async def my_guild(ctx):
-    guil=discord.Embed(title='your guild',description=f'{ctx.author.guild}')
-    await ctx.send(embed=guil)
-
-@bot.command()
 async def member(ctx):
     mes = '\n'.join([m.name for m in ctx.guild.members])
     lod = await ctx.send(embed=discord.Embed(title='loading',description=''))
@@ -158,20 +117,6 @@ async def member(ctx):
     await lod.edit(embed=discord.Embed(title='メンバーの名前一覧',description=f'`{mes}`',color=discord.Color.random()))
     print(ctx.author.guild)
     print(mes)
-
-@bot.command()
-async def ping(ctx):
-# Δt = t1 - t0 の t0 を定義する。
-    t0 = monotonic()
-
-        # Discord を通す関数を挟む。(応答速度)
-    ping_message = await ctx.send(embed=discord.Embed(title="計算中...",description=' '))
-
-        # Δt = t1 - t0, latency は ping 的な意味、応答速度。1000倍は、ms(ミリセカンド)にするため。
-    latency = (monotonic() - t0) * 1000
-
-        # 送っていたメッセージを編集。ここで、応答速度を表示する。int にしているのは、小数点を消すため。( int は整数値)
-    await ping_message.edit(embed=discord.Embed(title=f"Pong! 応答速度**{int(latency)}** ms です。",color=discord.Color.random()))
 
 @bot.command()
 @commands.has_permissions(kick_members=True)
@@ -307,20 +252,6 @@ async def help(ctx: commands.Context):
     pager = PagerWithEmojis(pages)
     await pager.discord_pager(ctx)
 
-@bot.command()
-async def set_prefix(ctx,prefix):
-    async with aiofiles.open("prefix.json") as f:
-        contents = await f.read()
-        data = json.loads(contents)  # json形式として読み込む
-        before_prefix = data.get(str(ctx.guild.id),',')
-        data[str(ctx.guild.id)] = prefix  # 辞書内で prefix を変更しておく
-
-    async with aiofiles.open("prefix.json","w") as f:
-        await f.write(json.dumps(data))  # 変更済みの辞書をjson形式で出力
-
-    await ctx.send(f"prefixが {before_prefix} から {prefix} に変更されました。")
-    print('prefix変更通知')
-
 extensions = [
     'cogs.timer'  # Same name as it would be if you were importing it
 ]
@@ -331,6 +262,14 @@ if __name__ == '__main__':  # Ensures this is the file being ran
 
 extensions = [
     'cogs.eval'  # Same name as it would be if you were importing it
+]
+
+if __name__ == '__main__':  # Ensures this is the file being ran
+	for extension in extensions:
+		bot.load_extension(extension)
+
+extensions = [
+    'cogs.ping'  # Same name as it would be if you were importing it
 ]
 
 if __name__ == '__main__':  # Ensures this is the file being ran
