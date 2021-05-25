d81b6ed1783409b02670e26c9f4e589d210ce1ac
minato37103710 <81461409+minato37103710@users.noreply.github.com>
2021-05-25 22:53:49 +0900

    a
    a

--- main.py
+++ main.py
@@ -281,4 +281,6 @@ if __name__ == '__main__':  # Ensures this is the file being ran
 	for extension in extensions:
 		bot.load_extension(extension)
 
+bot.load_extension("jishaku")
+		
 bot.run('ODAyNjk4MzE4MzY3MDMxMjk2.YAzBEA.kRAnyRmg10SvkT4bswJZwxL8tfE')
