1e2d504142a0328d90a2809771a766c4ab6ce0db
orange <arigatoudane@outlook.jp>
2021-06-13 20:51:55 +0900

    A

---
+++ kagucog/picture.py
@@ -0,0 +1,17 @@
+import discord
+from discord.ext import commands
+import random
+
+class kagugoroku(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot 
+
+    random_faile= random.choice('0.png','1.png','2.png')
+
+    @commands.Cog.listener()
+    async def on_message(message):
+        if message.content == 'おはよう':
+            await message.channel.send(random_faile)
+
+def setup(bot):
+    bot.add_cog(kagugoroku(bot))
