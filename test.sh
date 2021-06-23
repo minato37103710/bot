74574c6329b025a9e53abb7fb712bb104f39631c
orange <arigatoudane@outlook.jp>
2021-06-23 19:35:12 +0900

    A

--- test.sh
+++ test.sh
@@ -1,29 +1,3 @@
 #!/bin/bash
-
-# Args
-git_dir="$HOME/gh"
-filename=".autopull"
-
-# Sort 
-listfile="$git_dir/$filename"
-sort -o $listfile $listfile
-
-while read -r line; do
-
-    local_repo="${git_dir}/${line}"
-    if [ -d "${local_repo}" ]; then
-        # Pull --rebase
-        cd "${local_repo}"
-        echo "Starting git pull for repo: ${line}"
-        echo "Current branch: $(git branch | grep \* | cut -d ' ' -f2)"
-        git pull --rebase
-        echo ""
-    else
-        # Clone
-        cd "${git_dir}"
-        repo="git@github.com:blacklane/$line.git"
-        echo "Starting git clone for repo: $line assuming $repo"
-        git clone $repo
-        echo ""
-    fi
-done < "$listfile"
+cd /home/git/test/
+git --git-dir=.git pull origin master
