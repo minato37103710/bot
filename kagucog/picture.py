8f410687a5076be759c1960eebd3715261be516b
orange <arigatoudane@outlook.jp>
2021-06-27 18:21:28 +0900

    A

--- kagucog/picture.py
+++ kagucog/picture.py
@@ -16,12 +16,12 @@ class kagugoroku(commands.Cog):
         if message.author.bot:
             return
 
-        if message.content == 'おはよう':
+        if message.content == 'ひま':
         # Embedを使ったメッセージ送信 と ランダムで要素を選択
             images = glob.glob("*.png")
             random_image = random.choice(images)
             a=random.choice(('0.png', '1.png', '2.png', '3.png','4.png','5.png','6.png','7.png','8.png','9.png','10.png','11.png','12.png','13.png'))
-            await message.channel.send('語録あげる')
+            await message.channel.send(f'{message.author.mention}暇なら語録でも見てろ')
             await message.channel.send(file=discord.File(random_image))
             
             
