bbdd4ab58a94fe112cb130e0a756808b3c0a6d94
orange <arigatoudane@outlook.jp>
2021-07-29 21:20:45 +0900

    A

---
+++ turuhashicogs/event/URLBlocker.py
@@ -0,0 +1,20 @@
+import discord
+from discord.ext import commands
+import glob, random
+from tinydb import TinyDB, Query
+import asyncio
+
+class listener(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot 
+
+    @commands.Cog.listener()
+    async def on_message(self,msg):
+        
+        if 'https://' in msg.content:
+            print('A')
+            await msg.channel.purge(limit=1)
+            await msg.channel.send(f'URLを検知したため削除いたしました\n送信者:{msg.author.mention}')
+
+def setup(bot):
+    bot.add_cog(listener(bot))
