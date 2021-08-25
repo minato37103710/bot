65fe7a77b9dba27698a95aa20978ed211d18dea2
minato37103710 <81461409+minato37103710@users.noreply.github.com>
2021-08-25 23:10:06 +0900

    Update URLBlocker.py

--- turuhashicogs/event/URLBlocker.py
+++ turuhashicogs/event/URLBlocker.py
@@ -35,7 +35,7 @@ class listener(commands.Cog):
             
             else:
                 await msg.channel.purge(limit=1)
-                await msg.channel.send(f'URLを検知したため削除いたしました\n送信者:{msg.author.mention}\n対象url:{msg.content.replace("http", "httpリンク")}')
+                await msg.channel.send(f'URLを検知したため削除いたしました\n送信者:{msg.author.mention}\n対象url:{msg.content.replace("https//", "httpsリンク")}')
                 if len(db.search(User.name==msg.author.id)) > 0:
                     pass
                                 
