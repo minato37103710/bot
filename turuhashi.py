3f41f15598dd708b5b5f1c66aee6dff39782b054
minato37103710 <81461409+minato37103710@users.noreply.github.com>
2021-08-26 18:12:45 +0900

    Update turuhashi.py

--- turuhashi.py
+++ turuhashi.py
@@ -1,22 +1,12 @@
-import os
 import discord
-from discord.ext import commands
-from discord.ext import tasks
-import asyncio
-import requests
-from pprint import pprint
-import aiohttp 
-import random 
+from discord.ext import commands,tasks
+
 
 intent=discord.Intents.all()
 
 bot=commands.Bot(command_prefix='!',case_insensitive=True,intents=intent)
 
 bot.author_id=757106917947605034
-
-@bot.event
-async def on_ready():
-  print('on')
 
 bot.load_extension('jishaku')
 
