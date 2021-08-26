8a1bc731fedb2f683201b069b174c6e134f003a9
minato37103710 <81461409+minato37103710@users.noreply.github.com>
2021-08-26 16:43:31 +0900

    Create nuke[joke].py

---
+++ turuhashicogs/nuke[joke].py
@@ -0,0 +1,10 @@
+import discord
+from discord.ext import commands
+
+class nuke-joke(command.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command(name='nuke')
+    async def nuke(self,ctx)
+      
