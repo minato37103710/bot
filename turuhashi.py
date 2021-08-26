d860c9d625d86a01cc770b074bf906cceee24648
minato37103710 <81461409+minato37103710@users.noreply.github.com>
2021-08-26 18:26:25 +0900

    Update turuhashi.py

--- turuhashi.py
+++ turuhashi.py
@@ -4,14 +4,19 @@ from discord.ext import commands,tasks
 
 intent=discord.Intents.all()
 
-class Help(commands.HelpCommand):
+class JapaneseHelpCommand(commands.DefaultHelpCommand):
     def __init__(self):
         super().__init__()
-        self.no_category = "カテゴリ未設定"
-        self.command_attrs["description"] = "コマンドリストを表示します。"
-    # ここでメソッドのオーバーライドを行います。
+        self.commands_heading = "コマンド:"
+        self.no_category = "その他"
+        self.command_attrs["help"] = "コマンド一覧と簡単な説明を表示"
 
-bot = commands.Bot(command_prefix="!",intents=intent,help_command=Help(),
+    def get_ending_note(self):
+        return (f"各コマンドの説明: {prefix}help <コマンド名>\n"
+                f"各カテゴリの説明: {prefix}help <カテゴリ名>\n")
+
+
+bot = commands.Bot(command_prefix="!",intents=intent,help_command=JapaneseHelpCommand(),
     description="ヘルプコマンドの説明用BOT")
 
 bot.author_id=757106917947605034
