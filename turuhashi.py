4cb5c8c11839067bacada9ee707a8bd178a4f563
minato37103710 <81461409+minato37103710@users.noreply.github.com>
2021-08-26 18:18:00 +0900

    Update turuhashi.py

--- turuhashi.py
+++ turuhashi.py
@@ -4,7 +4,15 @@ from discord.ext import commands,tasks
 
 intent=discord.Intents.all()
 
-bot=commands.Bot(command_prefix='!',case_insensitive=True,intents=intent)
+class Help(commands.HelpCommand):
+    def __init__(self):
+        super().__init__()
+        self.no_category = "カテゴリ未設定"
+        self.command_attrs["description"] = "コマンドリストを表示します。"
+    # ここでメソッドのオーバーライドを行います。
+
+bot = commands.Bot(command_prefix="!",intents=intent,help_command=Help(),
+    description="ヘルプコマンドの説明用BOT")
 
 bot.author_id=757106917947605034
 
