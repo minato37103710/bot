2639c26cd55d2ea4a4cf9ce55edcc600565a102a
minato37103710 <81461409+minato37103710@users.noreply.github.com>
2021-08-26 17:29:03 +0900

    Update nuke[joke].py

--- turuhashicogs/nuke[joke].py
+++ turuhashicogs/nuke[joke].py
@@ -8,12 +8,12 @@ class nuke_joke(commands.Cog):
     @commands.command(name='nuke')
     async def nuke(self,ctx):
         channel=ctx.channel
-        await ctx.send('5秒以内に確認と送信してね')
+        await ctx.send('8秒以内に確認と送信してね')
 
         def hello_check(m):
             return m.content == '確認' and m.channel == channel
 
-        msg = await self.bot.wait_for('message', check=hello_check,timeout=5)
+        msg = await self.bot.wait_for('message', check=hello_check,timeout=8)
         await channel.send(f'{ctx.author.mention}、え？何しようとしてるの？（）メッセージ全消しとか害悪すぎでしょw')
 
 def setup(bot):
