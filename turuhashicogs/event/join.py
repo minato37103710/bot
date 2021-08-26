40373de20b9599270228bafbe9eefd601e9f5e8d
minato37103710 <81461409+minato37103710@users.noreply.github.com>
2021-08-26 15:58:27 +0900

    Update join.py

--- turuhashicogs/event/join.py
+++ turuhashicogs/event/join.py
@@ -8,7 +8,7 @@ class member_join(commands.Cog):
     @commands.Cog.listener()
     async def on_member_join(self,member):
       ch1=self.bot.get_channel(866265731301376002)
-      ch2=self.bot.get_channel()
+      ch2=self.bot.get_channel(880344125042491402)
       usab = discord.Embed(title=f'{member}の詳細', description='詳細だよ', color=discord.Color.orange())
       usab.set_author(name=member.name, icon_url=member.avatar.url)
       usab.set_thumbnail(url=member.avatar.url)
