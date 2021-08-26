2ae1b52462e96d8fcd0d1004e0b61b52bd801d3c
minato37103710 <81461409+minato37103710@users.noreply.github.com>
2021-08-26 21:22:09 +0900

    Update turuhashi.py

--- turuhashi.py
+++ turuhashi.py
@@ -3,8 +3,24 @@ from discord.ext import commands,tasks
 
 
 intent=discord.Intents.all()
+prefix = '$'
 
-prefix='t!'
+
+class Greet(commands.Cog, name='あいさつ'):
+    def __init__(self, bot):
+        super().__init__()
+        self.bot = bot
+
+    @commands.command(name="こんにちは")
+    async def hello(self, ctx):
+        """出会いのあいさつをする"""
+        await ctx.send(f"どうも、{ctx.author.name}さん!")
+
+    @commands.command(name="さようなら")
+    async def goodbye(self, ctx):
+        """別れの挨拶をする"""
+        await ctx.send(f"じゃあね、{ctx.author.name}さん!")
+
 
 class JapaneseHelpCommand(commands.DefaultHelpCommand):
     def __init__(self):
@@ -18,8 +34,8 @@ class JapaneseHelpCommand(commands.DefaultHelpCommand):
                 f"各カテゴリの説明: {prefix}help <カテゴリ名>\n")
 
 
-bot = commands.Bot(command_prefix=prefix,intents=intent,help_command=JapaneseHelpCommand(),
-    description="ヘルプコマンドの説明用BOT")
+bot = commands.Bot(command_prefix=prefix, help_command=JapaneseHelpCommand())
+bot.add_cog(Greet(bot=bot))
 
 bot.author_id=757106917947605034
 
