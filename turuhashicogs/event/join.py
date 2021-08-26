bd1618965652b6e68ee10cd6d6a3975ab92e399d
minato37103710 <81461409+minato37103710@users.noreply.github.com>
2021-08-26 15:56:03 +0900

    Update join.py

--- turuhashicogs/event/join.py
+++ turuhashicogs/event/join.py
@@ -7,8 +7,17 @@ class member_join(commands.Cog):
 
     @commands.Cog.listener()
     async def on_member_join(self,member):
-      ch=self.bot.get_channel(866265731301376002)
-      await ch.send(f'よろしくお願いします！\nもしよければ、ホワイトリスト申請のところに自分のゲーマータグを書いて、新規さんガイドを読んでください！')
-
+      ch1=self.bot.get_channel(866265731301376002)
+      ch2=self.bot.get_channel()
+      usab = discord.Embed(title=f'{member}の詳細', description='詳細だよ', color=discord.Color.orange())
+      usab.set_author(name=member.name, icon_url=member.avatar.url)
+      usab.set_thumbnail(url=member.avatar.url)
+      usab.add_field(name='名前', value=f'**{member.display_name}#{member.discriminator}**')
+      usab.add_field(name='あなたはBot?', value=member.bot)
+      usab.add_field(name='ID', value=member.id)
+      usab.add_field(name='作成時間', value=member.created_at)
+      usab.add_field(name='サーバーに参加した時間', value=member.joined_at)
+      await ch1.send(f'よろしくお願いします！\nもしよければ、ホワイトリスト申請のところに自分のゲーマータグを書いて、新規さんガイドを読んでください！')
+      await ch2.send(embed=usab)
 def setup(bot):
     bot.add_cog(member_join(bot))
