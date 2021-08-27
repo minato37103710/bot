e9ab7092ed4e1e7613daa299650deee625cd4fd0
minato37103710 <81461409+minato37103710@users.noreply.github.com>
2021-08-27 10:05:33 +0900

    Create pr.py

---
+++ turuhashicogs/pr.py
@@ -0,0 +1,11 @@
+import discord
+from discord.ext import commands
+import asyncio
+
+class pr(commands.Cog,name='紹介')
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command(name='pr',description='国家紹介文が表示されます')
+    async def pr(self,ctx):
+        await 
