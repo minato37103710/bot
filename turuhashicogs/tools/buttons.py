e4708e6c5235b0623ffee609e29f653a8ee7ed00
minato37103710 <81461409+minato37103710@users.noreply.github.com>
2021-08-28 20:39:57 +0900

    Create buttons.py

---
+++ turuhashicogs/tools/buttons.py
@@ -0,0 +1,29 @@
+import discord
+from discord.ext import commands
+
+class buttons(commands.Cog,name='ボタン'):
+    
+     def __init__(self,bot):
+         self.bot=bot
+
+     async def test_interaction(view, button, interaction):
+    # Push me!が押されたら。
+       await interaction.channel.send("Pushed button.")
+
+     async def test_count(view, button, interaction):
+    # 数字が押されたら。
+       button.label = str(int(button.label) + 1)
+       await interaction.message.edit(view=view)
+
+     @commands.command(name="_componesy_test")
+     async def test(ctx):
+    # ViewをTestViewっていう名前で作る。
+      view = componesy.View("TestView")
+    # 上の二つを登録する。
+      view.add_item("button", test_interaction, label="Push me!")
+      view.add_item("button", test_count, label="0")
+    # viewを送信する。
+      await ctx.reply("test", view=view)
+
+def setup(bot):
+  bot.add_cog(ping(bot))
