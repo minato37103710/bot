19462e74e8c745f90c207ad5163d0f54bba9cd7d
minato37103710 <81461409+minato37103710@users.noreply.github.com>
2021-08-29 14:31:01 +0900

    Update manu.py

--- turuhashicogs/tools/manu.py
+++ turuhashicogs/tools/manu.py
@@ -51,43 +51,43 @@ class manus(commands.Cog ,name='メニューです'):
         overwrites = create_overwrites(ctx, *objects)
 
         await ctx.guild.create_text_channel(name,overwrites=overwrites,
-        topic='Top secret text channel. Any leakage of this channel may result in serious trouble.',
-        reason='Very secret business.',
-    )
+            topic='Top secret text channel. Any leakage of this channel may result in serious trouble.',
+            reason='Very secret business.',
+        )
 
-@secret.command()
-@commands.guild_only()
-async def voice(ctx: commands.Context, name: str, *objects: typing.Union[discord.Role, discord.Member]):
-    """This does the same thing as the `text` subcommand
-    but instead creates a voice channel.
-    """
+    @secret.command()
+    @commands.guild_only()
+    async def voice(ctx: commands.Context, name: str, *objects: typing.Union[discord.Role, discord.Member]):
+        """This does the same thing as the `text` subcommand
+        but instead creates a voice channel.
+        """
 
-    overwrites = create_overwrites(ctx, *objects)
+        overwrites = create_overwrites(ctx, *objects)
 
-    await ctx.guild.create_voice_channel(
-        name,
-        overwrites=overwrites,
-        reason='Very secret business.'
-    )
+        await ctx.guild.create_voice_channel(
+            name,
+            overwrites=overwrites,
+            reason='Very secret business.'
+        )
 
-@secret.command()
-@commands.guild_only()
-async def emoji(ctx: commands.Context, emoji: discord.PartialEmoji, *roles: discord.Role):
-    """This clones a specified emoji that only specified roles
-    are allowed to use.
-    """
+    @secret.command()
+    @commands.guild_only()
+    async def emoji(ctx: commands.Context, emoji: discord.PartialEmoji, *roles: discord.Role):
+        """This clones a specified emoji that only specified roles
+        are allowed to use.
+        """
 
-    # fetch the emoji asset and read it as bytes.
-    emoji_bytes = await emoji.read()
+        # fetch the emoji asset and read it as bytes.
+        emoji_bytes = await emoji.read()
 
-    # the key parameter here is `roles`, which controls
-    # what roles are able to use the emoji.
-    await ctx.guild.create_custom_emoji(
-        name=emoji.name,
-        image=emoji_bytes,
-        roles=roles,
-        reason='Very secret business.'
-    )
-
-
-bot.run('token')
+        # the key parameter here is `roles`, which controls
+        # what roles are able to use the emoji.
+        await ctx.guild.create_custom_emoji(
+            name=emoji.name,
+            image=emoji_bytes,
+            roles=roles,
+            reason='Very secret business.'
+        )
+     
+setup def(bot)
+  bot.add_cog(menus(bot))
