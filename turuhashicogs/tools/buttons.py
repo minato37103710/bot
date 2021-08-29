1a622bec7f9e866120f05052929fff8c041fa7cc
minato37103710 <81461409+minato37103710@users.noreply.github.com>
2021-08-29 12:53:55 +0900

    Update buttons.py

--- turuhashicogs/tools/buttons.py
+++ turuhashicogs/tools/buttons.py
@@ -14,7 +14,7 @@ class Confirm(discord.ui.View):
     async def goo(self, button: discord.ui.Button, interaction: discord.Interaction):
         j=['グー✊','チョキ✌','パー✋']
         a=random.choice(j)
-        await interaction.response.send_message(f'{a}', ephemeral=True)
+        await interaction.response.send_message(f'{a}', ephemeral=False)
         self.value = True
 
     # This one is similar to the confirmation button except sets the inner value to `False`
@@ -22,14 +22,14 @@ class Confirm(discord.ui.View):
     async def scissors(self, button: discord.ui.Button, interaction: discord.Interaction):
         j=['グー✊','パー✋','チョキ✌']
         a=random.choice(j)
-        await interaction.response.send_message(f'{a}', ephemeral=True)
+        await interaction.response.send_message(f'{a}', ephemeral=False)
         self.value = True
         
     @discord.ui.button(label='✋', style=discord.ButtonStyle.grey)
     async def par(self, button: discord.ui.Button, interaction: discord.Interaction):
         j=['グー✊','パー✋','チョキ✌']
         a=random.choice(j)
-        await interaction.response.send_message(f'{a}', ephemeral=True)
+        await interaction.response.send_message(f'{a}', ephemeral=False)
         self.value = True
 
 class buttons(commands.Cog,name='ボタン'):
