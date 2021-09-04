5f2ae0dcf045a5d77eccb2affb5be16b7d55265e
minato37103710 <81461409+minato37103710@users.noreply.github.com>
2021-09-04 16:24:05 +0900

    Create wakeu.py

---
+++ turuhashicogs/wakeu.py
@@ -0,0 +1,23 @@
+import discord
+from discord.ext import commands
+
+class weakup(commands.Cog):
+    """These are the developer commands"""
+
+    def __init__(self, bot):
+        self.bot = bot
+  
+    @commands.Cog.listener()
+    async def on_ready(self):
+# cogsフォルダにあるエクステンションを読み込む。
+        for path in listdir("cogs"):
+            if path[0] in ("#", "."):
+                continue
+            if path.endswith(".py"):
+                bot.load_extension("cogs." + path[:-3])
+            elif "." not in path and path != "__pycache__" and path[0] != ".":
+                bot.load_extension("cogs." + path)
+            bot.print("Loaded extension", path)
+            
+def setup(bot):
+	bot.add_cog(weakup(bot))            
