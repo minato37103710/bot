4bd8705e83280a6ca092b5e5fd93296b45bb22de
minato37103710 <81461409+minato37103710@users.noreply.github.com>
2021-09-04 13:14:57 +0900

    Update message.py

--- turuhashicogs/event/message.py
+++ turuhashicogs/event/message.py
@@ -11,8 +11,9 @@ class nasuyaki(commands.Cog):
         return
 
       if 'なす焼き' in msg.content or 'ナス焼き' in msg.content or 'なすやき' in msg.content:
-        nasu=random.choice(['なす焼き！（熱盛風）','<:20210831182900711:882779002903879720>'])
-        await msg.channel.send()
+        nasu=['なす焼き！（熱盛風）','<:20210831182900711:882779002903879720>']
+        yaki=random.choice(nasu)
+        await msg.channel.send(yaki)
 
 def setup(bot):
     bot.add_cog(nasuyaki(bot))
