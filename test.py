59d5dcc9a234f49c6db946b361bda5af923fae90
minato37103710 <81461409+minato37103710@users.noreply.github.com>
2021-09-05 00:01:53 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 import os
 import discord
 from discord.ext import commands
-
+import asyncio
 bot=commands.Bot(command_prefix='Test!',intents=discord.Intents.all())
 
 @bot.event
@@ -12,6 +12,7 @@ async def on_ready():
 @bot.event
 async def on_message(msg):
   if msg.content=='1234567890':
+    await asyncio.sleep(1)
     await msg.channel.send('1234567890')
 
 bot.load_extension('jishaku')
