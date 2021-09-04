e174ce37f1676bb35819a041b70efa6ffa76df24
minato37103710 <81461409+minato37103710@users.noreply.github.com>
2021-09-04 22:57:37 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,3 +1,12 @@
 import os
+import discord
+from discord.ext import commands
 
-print(os.getenv('token'))
+bot=commands.Bot(command_prefix='Test!',intents=discord.Intents.all())
+
+@bot.event
+async def on_ready():
+  print(f'{bot.user} is online')
+  print(bot.id)
+
+bot.run(os.getenv('token'))
