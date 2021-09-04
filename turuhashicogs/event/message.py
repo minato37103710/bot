5f69ab6c08c091e18aefa70d02addbde2cfcf0d9
minato37103710 <81461409+minato37103710@users.noreply.github.com>
2021-09-04 10:37:29 +0900

    Create message.py

---
+++ turuhashicogs/event/message.py
@@ -0,0 +1,14 @@
+import discord
+from discord.ext import commands
+
+class message_listener(commands.Cog)
+    def __init__(self,bot):
+      self.bot=bot
+
+    @commands.Cog.listener()
+    async def listenr(self,msg):
+      if 'なす焼き' in msg.content or 'ナス焼き' in msg.content or 'なすやき' in msg.content:
+        await msg.channel.send('なす焼き！（熱盛風）')
+
+def setup(bot):
+    bot.add_cog(message_listenr(bot))
