8ee94e45941fb85e11e153300109516ad24f87ab
minato37103710 <81461409+minato37103710@users.noreply.github.com>
2021-09-04 23:59:57 +0900

    Update test.py

--- test.py
+++ test.py
@@ -9,5 +9,10 @@ async def on_ready():
   print(f'{bot.user} is online')
   print(bot.user.id)
 
+@bot.event
+async def on_message(msg):
+  if msg.content=='1234567890':
+    await msg.channel.send('1234567890')
+
 bot.load_extension('jishaku')
 bot.run(os.getenv('token'))
