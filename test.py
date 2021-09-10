177a0fdf4f34cfe7a7f5dec82f923b9a39fd7037
minato37103710 <81461409+minato37103710@users.noreply.github.com>
2021-09-10 18:11:57 +0900

    Update test.py

--- test.py
+++ test.py
@@ -9,11 +9,9 @@ async def on_ready():
   print(f'{bot.user} is online')
   print(bot.user.id)
 
-@bot.event
-async def on_message(msg):
-  if msg.content=='1234567890':
-    await asyncio.sleep(1)
-    await msg.channel.send('1234567890')
+@bot.commands(name='test')
+async def test(ctx,aaa):
+  await ctx.send(aaa)
 
 bot.load_extension('jishaku')
 bot.run(os.getenv('token'))
