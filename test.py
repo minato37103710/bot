06e35c9b8eb5f997bdbb685d4acc49a7d91f38c4
minato37103710 <81461409+minato37103710@users.noreply.github.com>
2021-09-10 18:13:55 +0900

    Update test.py

--- test.py
+++ test.py
@@ -9,7 +9,7 @@ async def on_ready():
   print(f'{bot.user} is online')
   print(bot.user.id)
 
-@bot.commands(name='test')
+@bot.command(name='test')
 async def test(ctx,aaa):
   await ctx.send(aaa)
 
