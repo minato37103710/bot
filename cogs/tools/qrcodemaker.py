728b4038296cf5fd180a4404f5239ee411fc880c
minato37103710 <81461409+minato37103710@users.noreply.github.com>
2021-09-14 15:16:44 +0900

    Update qrcodemaker.py

---
+++ cogs/tools/qrcodemaker.py
@@ -0,0 +1,21 @@
+import qrcode
+import discord
+from discord.ext import commands
+
+class qrcodemaker(commands.Cog):
+    def __init__(self,bot):
+         self.bot=bot
+
+    @commands.command(name='make_qr',aliases=['mkqr'])
+    async def qrcodemake(self,ctx,*,arg):
+        img = qrcode.make(arg)
+        print(arg)
+        print(type(img))
+        print(img.size)
+        # <class 'qrcode.image.pil.PilImage'>
+        # (290, 290)
+        img.save('cogs/img/_qrcode.png')
+        await ctx.send(file=discord.File('cogs/img/_qrcode.png'))
+
+def setup(bot):
+    bot.add_cog(qrcodemaker(bot))
