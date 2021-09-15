a0a07221d0ff11d272779454adef9a6369fe3a13
minato37103710 <81461409+minato37103710@users.noreply.github.com>
2021-09-15 09:05:03 +0900

    Update janome-test.py

--- janome-test.py
+++ janome-test.py
@@ -118,7 +118,7 @@ async def on_message(message):
     if message:    
         text = message.content
         res = make_reply(text)
-        if res == null:
+        if not res:
             return
         else:
             await message.channel.send(res)
