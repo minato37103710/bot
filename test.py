d61f6dddff04f9e6c2afe337d465f0fd4d4f44c4
minato37103710 <81461409+minato37103710@users.noreply.github.com>
2021-09-18 17:28:40 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,29 +1,11 @@
+from janome.analyzer import Analyzer
 from janome.tokenizer import Tokenizer
- 
-path = './test.txt' #同じディレクトリにtext.txtを配置
-file = open(path, encoding="utf-8")  #ファイルをオープン Windows or Mac
-#file = open(path)  #ファイルをオープン Mac
-bindata = file.read()#開いたファイルの読み込み
-#textdata = bindata.decode('shift_jis')
- 
-t = Tokenizer() #形態素解析
-dic = {} #辞書
-#---------------------------------------------------------------------------------------------
-lines = bindata.split("\r\n") #改行で分割                              手順1
-#---------------------------------------------------------------------------------------------
-for line in lines:  #分割された文章毎に繰り返す
-#-----------------------------------------------------------------------------
-    malist = t.tokenize(line)  #形態素のリスト
-    for w in malist:  # リストの各要素を取り出してカウント
-        word = w.surface  #                                          手順2         手順4
-        part = w.part_of_speech  #品詞
-        if part.find('名詞') and lt; 0: continue
-#-----------------------------------------------------------------------------
-        if not word in dic:
-            dic[word] = 0  #数を格納するカウンター用の変数を生成  #        手順3
-            dic[word] += 1  # カウンターを増やす
-#---------------------------------------------------------------------------------------------
-dic = sorted(dic.items(),key = lambda x:x[1], reverse=True)  #       手順5
-#---------------------------------------------------------------------------------------------
-for word, cnt in keys[:20]:
-    print("{0}({1})\n".format(word,cnt), end="")
+from janome.tokenfilter import POSKeepFilter
+
+text = '蛇の目はPure Ｐｙｔｈｏｎな形態素解析器です。'
+tokenizer = Tokenizer()
+token_filters = [POSKeepFilter(['名詞'])]
+a = Analyzer([], tokenizer, token_filters)
+
+for token in a.analyze(text):
+  print(token)
