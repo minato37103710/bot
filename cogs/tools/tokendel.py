f471ad428a48bb2c4e595739b58dae8d41d25054
minato37103710 <81461409+minato37103710@users.noreply.github.com>
2022-01-15 13:20:47 +0900

    Create tokendel.py

---
+++ cogs/tools/tokendel.py
@@ -0,0 +1,17 @@
+import discord
+from dicord.ext import commands
+from re import findall
+
+class tokendel(commands.Cog):
+      def __init__(self, bot):
+        self.bot = bot 
+        
+      def check_token(self, content: str) -> bool:
+        return bool(findall("([a-zA-Z]{24}).([a-zA-Z]{6}).([a-zA-Z]{27})", content))
+      @commands.Cog.listener()
+      async def on_message(self,message):
+        if self.check_token(message.content):
+          await meessage.author.ban(reason="token爆したから自動BAN")
+
+def setup(bot):
+    bot.add_cog(tokendel(bot))
